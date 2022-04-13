674495d9b996d02b742605bad6102119a55e9b59
Lautaro Mariani Aguirre <91752333+Lautaro2210@users.noreply.github.com>
2022-04-13 15:24:26 -0300

    PRUEBA 1 FINALIZADA

--- Assets/Prueba1.cs
+++ Assets/Prueba1.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Prueba1 : MonoBehaviour
 {
+    public int taxis;
+    public int dias;
+    public int lluvia;
     // Start is called before the first frame update
     void Start()
     {
-        
+        int combustible;
+        combustible = 130;
+        int total;
+        int lluv;
+        int resto;
+
+        resto = dias - lluvia;
+        lluv = 120 / 15;
+        total = taxis * ((lluvia * (combustible * lluv)) + (resto * (combustible * 6)));
+
+        if (dias < 5)
+        {
+            Debug.Log("Error");
+        }
+
+        else if (lluvia < 0)
+        {
+            Debug.Log("Error");
+        }
+        else if (lluvia > dias)
+        {
+            Debug.Log("Error");
+        }
+        else
+        {
+            Debug.Log("Una flota de " + taxis + " unidades trabajando durante " + dias + "dias implicara un gasto de" + total + "pesos en concepto de comsutible");
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
